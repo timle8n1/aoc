463cbe03ef9f05410d88102e3b989c0ff083cced
Tim LeMaster <tim@thelemasters.net>
2022-12-03 10:28:58 -0500

    refactor aoc 2022 day 3

--- aoc2022/day3/solve.rb
+++ aoc2022/day3/solve.rb
@@ -1,28 +1,24 @@
 #! /usr/bin/env ruby
 
+class String
+    def priority
+        self.ord >= "a".ord ? self.ord - "a".ord + 1 : self.ord - "A".ord + 1 + 26
+    end
+end
+
 module Solver
     def self.solve1(input)
         return input.map { |line| 
             line.chomp.chars.each_slice(line.size / 2).map(&:to_a).reduce(&:&)[0]
-        }.reduce(0) { |sum, n| sum + Solver.priority(n)}
+        }.map(&:priority).reduce(&:+)
     end
 
     def self.solve2(input)
         return input.each_slice(3).map { |lines| 
             lines.map { |line| line.chomp.chars.to_a }.reduce(&:&)[0]
-        }.reduce(0) { |sum, n| sum + Solver.priority(n)}
-    end
-
-    def self.priority(input)
-        inputAsciiValue = input.ord
-        if inputAsciiValue >= "a".ord 
-            return input.ord - "a".ord + 1
-        else 
-            return input.ord - "A".ord + 1 + 26
-        end
+        }.map(&:priority).reduce(&:+)
     end
 end
-
 
 input = File.readlines(ARGV[0])
 puts Solver.solve1(input)
