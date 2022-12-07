71776734f02152c9d6e89bf94040c069b02a53d4
Tim LeMaster <tim@thelemasters.net>
2022-12-07 14:12:14 -0500

    Add one liner

--- aoc2022/day6/solve.rb
+++ aoc2022/day6/solve.rb
@@ -11,8 +11,16 @@ module Solver
             window.append(c[i])
         end
     end
+
+    def self.oneLine(c, marker_length)
+        (0..c.length - marker_length - 1).each { |i| return i + marker_length if Set.new(c[i, marker_length]).length == marker_length }
+    end
 end
 
 c = File.readlines(ARGV[0])[0].chars
 puts Solver.solve(c, 4)
 puts Solver.solve(c, 14)
+
+puts Solver.oneLine(c, 4)
+puts Solver.oneLine(c, 14)
+
